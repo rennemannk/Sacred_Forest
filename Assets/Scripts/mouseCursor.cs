fe6c595cc2736705ba9175549073db1967c57eae
angelicaroque2929 <58479654+angelicaroque2929@users.noreply.github.com>
2021-04-07 17:38:47 -0400

    Angie(Cursor)

---
+++ Assets/Scripts/mouseCursor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseCursor : MonoBehaviour
+{
+    void Start(){
+        Cursor.visible = false;
+    }
+
+    void Update (){
+        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = cursorPos;
+    }
+}
