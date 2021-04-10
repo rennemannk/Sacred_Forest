c3567e0573d1d48a6a592f5d866498dd2b0d6ab2
ashleymonika22 <ashleymvdd@gmail.com>
2021-04-09 20:50:49 -0400

    Update Timer.cs

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -30,7 +30,7 @@ public class Timer : MonoBehaviour
                 Debug.Log("Time has run out!");
                 timeRemaining = 0;
                 timerIsRunning = false;
-                Application.LoadLevel (levelToLoad);
+                Application.LoadLevel(levelToLoad);
             }
         }
     }
